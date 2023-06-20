3344bf67d308cb7819715e31c613906d0993967b
Gabriel <legard2911@gmail.com>
2023-06-20 16:31:10 -0500

    Agregar CategoriaModel

---
+++ Proyecto_BD/src/main/java/clases/CategoriaModel.java
@@ -0,0 +1,33 @@
+package clases;
+
+import java.util.List;
+
+import interfaces.CategoriaInterface;
+
+public class CategoriaModel implements CategoriaInterface {
+
+	@Override
+	public int createSubject(Productos subject) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Productos> listSubject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Productos getSubject(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delateSubject(String id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
